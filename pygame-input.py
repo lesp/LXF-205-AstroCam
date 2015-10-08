b340031f565105e7d6c8da07cbf989610858c46c
Les Pounder <lespounder@gmail.com>
2015-10-08 20:29:00 +0100

    Added more code which uses functions triggered by keyboard events, SenseHat uses keyboard presses for the joystick

--- pygame-input.py
+++ pygame-input.py
@@ -8,11 +8,32 @@ def takepic():
     print('Taking picture...standby')
     time.sleep(1)
 
+def timer():
+    print('TIMER')
+    #Code to power the timer.
+    #Add 5 seconds
+    #Deduct 5 seconds
+
+def flash():
+    print('FLASH AHH HAA')
+    #Turn the flash on and off
+
 while True:
     print('Waiting')
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == pygame.K_UP:
-                print('Key Pressed')
+                print('Adding time')
+                timer()
+            elif event.key == pygame.K_DOWN:
+                print('Deducting time')
+                timer()
+            elif event.key == pygame.K_LEFT:
+                print('Add flash')
+                flash()
+            elif event.key == pygame.K_RIGHT:
+                print('Rainbow Flash')
+            elif event.key == pygame.K_RETURN:
+                print('Takepic')
                 takepic()
         time.sleep(0.1)
